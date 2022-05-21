921a369f59a3160bd3a1147a1dc1f06f13b9e50b
liangningcong <liangningcong@gmail.com>
2022-05-21 22:25:17 +0800

    jerryai-coding-test: update

--- jerryai-coding-test/index.js
+++ jerryai-coding-test/index.js
@@ -1,22 +1,45 @@
+/*
+思路：add 的时候，生成一个全部是 '0' 的字符串 string，长度是 range[1]；或者根据 range[1] 在现有的字符串后补 '0'，使得字符串的长度为 range[1]。
+  然后把 string[range[0]] ~ string[range[1]] 中的 '0' 全部转成 '1'。
+  即用连续的 1 表示一个 range。
+  对于负数 range 的情况，只需要在正数的基础上反转一下字符串即可。
+*/
+
 class RangeList {
   constructor() {
-    // a string of '0/1' representing a range. e.g. [1, 5) => 01111; [3, 5) => 00011
-    this.rangeString = '';
+    // a string of '0' or '1', representing a range. e.g. [1, 5) => 01111; [3, 5) => 00011
+    this.positive = '';
+    this.negative = '';
+  }
+
+  reverseString(string) {
+    return [...string].reverse().join('');
   }
 
   /**
-   * from 'start' to 'end'(not included), turns every charater in this.rangeString into 'bit'
+   * from 'start' to 'end'(not included), turns every charater in 'target' into 'bit'
    * @param {number} start
    * @param {number} end
    * @param {'0' | '1'} bit
    */
-  toggle(start, end, bit) {
+  toggle(target, start, end, bit) {
     const count = end - start;
     const reg = RegExp(`(\\d{${start}})(\\d{${count}})(\\d*)`, 'g');
 
-    this.rangeString = this.rangeString.replace(
-      reg,
-      (match, p1, p2, p3) => `${p1}${bit.repeat(count)}${p3}`
+    return target.replace(reg, (match, p1, p2, p3) => `${p1}${bit.repeat(count)}${p3}`);
+  }
+
+  handleAdd(target, range) {
+    if (range[1] > target.length) {
+      target = target.padEnd(range[1], '0');
+    }
+
+    return this.toggle(target, ...range, '1');
+  }
+
+  addNegative(target, range) {
+    return this.reverseString(
+      this.handleAdd(this.reverseString(target), [0 - range[1], 0 - range[0]])
     );
   }
 
@@ -25,12 +48,31 @@ class RangeList {
    * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
    */
   add(range) {
-    const count = range[1] - this.rangeString.length;
-    if (count > 0) {
-      this.rangeString += '0'.repeat(count);
+    if (range[0] > range[1]) {
+      [range[0], range[1]] = [range[1], range[0]];
     }
 
-    this.toggle(...range, '1');
+    if (range[0] >= 0) {
+      // range 中全部是正数
+      this.positive = this.handleAdd(this.positive, range);
+    } else if (range[1] < 0) {
+      // all negative
+      this.negative = this.addNegative(this.negative, range);
+    } else {
+      // from negative to positive
+      this.negative = this.addNegative(this.negative, [range[0], 0]);
+      this.positive = this.handleAdd(this.positive, [0, range[1]]);
+    }
+  }
+
+  handleRemove(target, range) {
+    return this.toggle(target, ...range, '0');
+  }
+
+  removeNegative(target, range) {
+    return this.reverseString(
+      this.handleRemove(this.reverseString(target), [0 - range[1], 0 - range[0]], '0')
+    );
   }
 
   /**
@@ -38,27 +80,78 @@ class RangeList {
    * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
    */
   remove(range) {
-    this.toggle(...range, '0');
+    if (range[0] > range[1]) {
+      [range[0], range[1]] = [range[1], range[0]];
+    }
+
+    if (range[0] >= 0) {
+      // range 中全部是正数
+      this.positive = this.handleRemove(this.positive, range);
+    } else if (range[1] < 0) {
+      // all negative
+      this.negative = this.removeNegative(this.negative, range);
+    } else {
+      // from negative to positive
+      this.negative = this.removeNegative(this.negative, [range[0], 0]);
+      this.positive = this.handleRemove(this.positive, [0, range[1]]);
+    }
+  }
+
+  /**
+   * turns string of '01's into ranges
+   * @param {string} target
+   * @param {boolean} isPositive
+   * @returns
+   */
+  findRanges(target, isPositive) {
+    // 利用正则找出所有连续的 '1'，以及他们的 index 和长度
+    const regexp = RegExp('(1+)', 'gd');
+
+    let ranges = [];
+    let result = regexp.exec(target);
+
+    while (result !== null) {
+      const indice = result.indices[0];
+      result = regexp.exec(target);
+
+      if (isPositive) {
+        ranges.push([indice[0], indice[1]]);
+      } else {
+        ranges.unshift([0 - indice[1], 0 - indice[0]]);
+      }
+    }
+
+    return ranges;
+  }
+
+  /**
+   * format
+   * @param {Array<[number, number]>} ranges
+   * @returns string
+   */
+  formatPrint(ranges) {
+    return ranges.reduce((prev, curr) => [...prev, `[${curr})`], []).join(' ');
   }
 
   /**
    * Prints out the list of ranges in the range list
    */
   print() {
-    // use a regexp to find all groups of '1' and their index in this.rangeString
-    const regexp = RegExp('(1+)', 'gd');
-    const rangeString = this.rangeString;
+    // 分别找出正数和负数的 ranges
+    const nRanges = this.findRanges(this.reverseString(this.negative));
+    const pRanges = this.findRanges(this.positive, true);
 
-    let ranges = [];
-    let result = regexp.exec(rangeString);
-
-    while (result !== null) {
-      const indice = result.indices[0];
-      ranges.push(`[${indice[0]}, ${indice[1]})`);
-      result = regexp.exec(rangeString);
+    // 如果负数的最后一个 range 与 正数第一个 range 存在连续性，则合并成一个 range
+    if (nRanges.length && pRanges.length) {
+      if (nRanges.slice(-1)[0][1] === pRanges[0][0]) {
+        const start = nRanges.pop()[0];
+        pRanges[0][0] = start;
+      }
     }
 
-    return ranges.join(' ');
+    const ranges = this.formatPrint([...nRanges, ...pRanges]);
+    console.log(ranges);
+    return ranges;
   }
 }
 
@@ -69,29 +162,29 @@ const rl = new RangeList();
 rl.add([1, 5]);
 rl.print();
 
-// rl.add([10, 20]);
-// rl.print();
+rl.add([20, 10]);
+rl.print();
 
-// rl.add([20, 20]);
-// rl.print();
+rl.add([20, 20]);
+rl.print();
 
-// rl.add([20, 21]);
-// rl.print();
+rl.remove([10, 10]);
+rl.print();
 
-// rl.add([2, 4]);
-// rl.print();
+rl.remove([10, 11]);
+rl.print();
 
-// rl.add([3, 8]);
-// rl.print();
+rl.add([-5, 0]);
+rl.print();
 
-// rl.remove([10, 10]);
-// rl.print();
+rl.add([-9, -5]);
+rl.print();
 
-// rl.remove([10, 11]);
-// rl.print();
+rl.add([6, 15]);
+rl.print();
 
-// rl.remove([15, 17]);
-// rl.print();
+rl.remove([-3, -9]);
+rl.print();
 
-// rl.remove([3, 19]);
-// rl.print();
+rl.remove([-1, 3]);
+rl.print();
--- jerryai-coding-test/test.js
+++ jerryai-coding-test/test.js
@@ -1,15 +1,16 @@
 const RangeList = require('./index');
 
-const rl = new RangeList();
 
 describe('RangeList.add', () => {
+  const rl = new RangeList();
+
   describe.each([
-    [[1, 5], '[1, 5)'],
-    [[10, 20], '[1, 5) [10, 20)'],
-    [[20, 20], '[1, 5) [10, 20)'],
-    [[20, 21], '[1, 5) [10, 21)'],
-    [[2, 4], '[1, 5) [10, 21)'],
-    [[3, 8], '[1, 8) [10, 21)'],
+    [[1, 5], '[1,5)'],
+    [[10, 20], '[1,5) [10,20)'],
+    [[20, 20], '[1,5) [10,20)'],
+    [[20, 21], '[1,5) [10,21)'],
+    [[2, 4], '[1,5) [10,21)'],
+    [[3, 8], '[1,8) [10,21)'],
   ])('add(%p)', (range, expected) => {
     test(`returns ${expected}`, () => {
       rl.add(range);
@@ -20,11 +21,52 @@ describe('RangeList.add', () => {
 });
 
 describe('RangeList.remove', () => {
+  const rl = new RangeList();
+
+  rl.add([1, 8]);
+  rl.add([10, 21]);
+
   describe.each([
-    [[10, 10], '[1, 8) [10, 21)'],
-    [[10, 11], '[1, 8) [11, 21)'],
-    [[15, 17], '[1, 8) [11, 15) [17, 21)'],
-    [[3, 19], '[1, 3) [19, 21)'],
+    [[10, 10], '[1,8) [10,21)'],
+    [[10, 11], '[1,8) [11,21)'],
+    [[15, 17], '[1,8) [11,15) [17,21)'],
+    [[3, 19], '[1,3) [19,21)'],
+  ])('remove(%p)', (range, expected) => {
+    test(`returns ${expected}`, () => {
+      rl.remove(range);
+
+      expect(rl.print()).toBe(expected);
+    });
+  });
+});
+
+describe('RangeList.add negative', () => {
+  const rl = new RangeList();
+
+  rl.add([1, 3]);
+  rl.add([19, 21]);
+
+  describe.each([
+    [[-5, -1], '[-5,-1) [1,3) [19,21)'],
+    [[-2, 1], '[-5,3) [19,21)'],
+  ])('add(%p)', (range, expected) => {
+    test(`returns ${expected}`, () => {
+      rl.add(range);
+
+      expect(rl.print()).toBe(expected);
+    });
+  });
+});
+
+describe('RangeList.remove negative', () => {
+  const rl = new RangeList();
+
+  rl.add([-5, 3]);
+  rl.add([19, 21]);
+
+  describe.each([
+    [[-5, -4], '[-4,3) [19,21)'],
+    [[-2, 1], '[-4,-2) [1,3) [19,21)'],
   ])('remove(%p)', (range, expected) => {
     test(`returns ${expected}`, () => {
       rl.remove(range);
